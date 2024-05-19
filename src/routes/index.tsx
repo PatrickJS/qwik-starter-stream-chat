df35fb141576efc03e8911e096d167413adc7d26
PatrickJS <github@gdi2290.com>
2024-05-18 19:18:45 -0700

    Initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,117 @@
+import {
+  component$,
+  useStore,
+  $,
+  JSXOutput,
+  useSignal,
+  useVisibleTask$,
+} from '@builder.io/qwik';
+import { server$ } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  const output = useStore<JSXOutput[]>([]);
+  const startStreaming = $(async () => {
+    const stream = await severStream();
+    for await (const jsx of stream) {
+      output.push(jsx);
+    }
+  });
+  return (
+    <>
+      <button onClick$={startStreaming}>start</button>
+      <div>{output.map((jsx) => jsx)}</div>
+    </>
+  );
+});
+
+const Counter = component$(() => {
+  const count = useSignal(123);
+  return <button onClick$={() => count.value++}>{count.value}</button>;
+});
+
+const Greeter = component$<{ name: string }>(({ name }) => {
+  return <span>Hello {name}!</span>;
+});
+
+const Clock = component$(() => {
+  const now = useSignal('');
+  // eslint-disable-next-line qwik/no-use-visible-task
+  useVisibleTask$(({ cleanup }) => {
+    const update = () => (now.value = new Date().toLocaleTimeString());
+    update();
+    setInterval(update, 1000);
+  });
+  return <span>Time: {now.value}</span>;
+});
+
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+let count = 0;
+const severStream = server$(async function* () {
+  const yo = count++;
+  yield (
+    <div>
+      {yo}
+      <span>Check out how streaming works in Qwik! </span>
+    </div>
+  );
+  await delay(500);
+  yield (
+    <div>
+      {yo}
+      <span>You can even stream components: </span>
+    </div>
+  );
+  yield (
+    <div>
+      {yo}
+      <Greeter name="World" />
+    </div>
+  );
+  await delay(500);
+  yield (
+    <div>
+      {yo}
+      <span>Or interactive components: </span>
+    </div>
+  );
+  yield (
+    <div>
+      {yo}
+      <Counter />
+    </div>
+  );
+  yield (
+    <div>
+      {yo}
+      <span>! </span>
+    </div>
+  );
+  await delay(500);
+  yield (
+    <div>
+      {yo}
+      <span>Or event self runnig ones: </span>
+    </div>
+  );
+  yield (
+    <div>
+      {yo}
+      <Clock />
+    </div>
+  );
+  yield (
+    <div>
+      {yo}
+      <span>! </span>
+    </div>
+  );
+  await delay(500);
+  yield (
+    <div>
+      {yo}
+      <span>
+        Ohh, and all of the code streams into the client (loads lazily)!
+      </span>
+    </div>
+  );
+});
